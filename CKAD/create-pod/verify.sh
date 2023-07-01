9ae29d67c004a3561897c94e2bf434c6efd0192c
james-clarusway <james@clarusway.com>
2023-07-01 22:48:50 +0300

    create-pod/

--- CKAD/create-pod/verify.sh
+++ CKAD/create-pod/verify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ $(kubectl get po -o yaml | grep '\- image:' | awk '{print $3}') = "nginx:latest" ]]
+if [[ $(kubectl get po -o yaml | grep '\- image:' | awk '{print $3}') = 'nginx:latest' \
+&& $(k get po mypod -o yaml | grep "tier" | awk '{print $1,$2}') = 'tier: frontend' \
+&& $(k get po mypod -o yaml | grep "mypod" | head -1 | awk '{print $2}') = 'mypod' ]]
 then
   exit 0
 else
