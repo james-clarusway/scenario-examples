c244f0582e30292061fe13351fbd3a5761845c18
james-clarusway <james@clarusway.com>
2023-07-02 11:21:59 +0300

    step-5

--- CKAD/basic-consepts/step3/verify.sh
+++ CKAD/basic-consepts/step3/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(kubectl get deploy -n webserver -o yaml | grep 'mydeploy' | head -1 | awk '{print $2}') = 'mydeploy' \
+if [[ $(kubectl get deploy -n webserver | grep 'mydeploy' | head -1 | awk '{print $1}') = 'mydeploy' \
 && $(kubectl get deploy mydeploy -n webserver -o yaml | grep 'httpd:alpine' | head -1 | awk '{print $3}') = 'httpd:alpine' \
 && $(kubectl get deploy mydeploy -n webserver -o yaml | grep 'web: apache' | head -1 | awk '{print $1,$2}') ]]
 then
