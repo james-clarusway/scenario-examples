4b9e6e6bee35ba430e8c722fae675f766f9b2a7f
james-clarusway <james@clarusway.com>
2023-07-14 17:59:40 +0300

    ingress

--- CKAD/networking/step4/verify.sh
+++ CKAD/networking/step4/verify.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [[ $(kubectl -n blue get ingress test-ingress -o jsonpath='{.spec.rules[0].host}') == 'ckad.clarusway.us' && \
-$(kubectl -n blue get ingress test-ingress -o jsonpath='{.spec.rules[0].http.paths[*].backend.service}') == '{"name":"nginx-svc","port":{"number":80}} {"name":"apache-svc","port":{"number":80}}' || '{"name":"apache-svc","port":{"number":80}} {"name":"nginx-svc","port":{"number":80}}'  ]]
-$(kubectl -n blue get ingress test-ingress -o jsonpath='{.spec.rules[0].http.paths[*].path}') == '/nginx /apache' || 'apache /nginx' ]]
+$(kubectl -n blue get ingress test-ingress -o jsonpath='{.spec.rules[0].http.paths[*].backend.service}') == '{"name":"nginx-svc","port":{"number":80}} {"name":"apache-svc","port":{"number":80}}' || '{"name":"apache-svc","port":{"number":80}} {"name":"nginx-svc","port":{"number":80}}' && \
+$(kubectl -n blue get ingress test-ingress -o jsonpath='{.spec.rules[0].http.paths[*].path}') == '/nginx /apache' || '/apache /nginx' ]]
+
 then
   exit 0
 else
