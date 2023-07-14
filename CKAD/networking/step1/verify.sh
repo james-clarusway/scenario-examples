b5c9ad2fa9fa431d44d0209e982f5b46f101bc95
james-clarusway <james@clarusway.com>
2023-07-15 00:06:22 +0300

    networking

--- CKAD/networking/step1/verify.sh
+++ CKAD/networking/step1/verify.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# if [[ $(kubectl get svc mynginx | grep 'mynginx' | awk '{print $1}') = 'mynginx' \
-# && $(kubectl get ep mynginx | grep 'mynginx' | awk -F: '{print $2}' | awk '{print $1}') = '80' ]]
-# then
-#   exit 0
-# else
-#   exit 1
-# fi
-
-
-ls
+if [[ $(kubectl get svc mynginx | grep 'mynginx' | awk '{print $1}') = 'mynginx' \
+&& $(kubectl get ep mynginx | grep 'mynginx' | awk -F: '{print $2}' | awk '{print $1}') = '80' ]]
+then
+  exit 0
+else
+  exit 1
+fi
