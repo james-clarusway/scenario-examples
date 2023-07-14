096e1798f7228559f430e5699e16f5a76550dcdf
james-clarusway <james@clarusway.com>
2023-07-14 12:26:24 +0300

    netpol

--- CKAD/networking/step4/verify.sh
+++ CKAD/networking/step4/verify.sh
@@ -2,7 +2,7 @@
 
 number=$(kubectl get deploy nginx-deploy | tail -n 1 | awk '{print $4}')
 
-if [[ $number -eq 3 ]]
+if [[ $(k get deployment nginx-deploy -o yaml | grep "replicas: 3" | head -1) -eq 3 ]]
 then
   exit 0
 else
