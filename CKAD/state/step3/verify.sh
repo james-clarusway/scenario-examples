bf5f380973c8087a2c79c9e17ddf39f4e4662f5d
james-clarusway <james@clarusway.com>
2023-07-15 17:38:37 +0300

    volumes

--- CKAD/state/step3/verify.sh
+++ CKAD/state/step3/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $(kubectl get po ng-pod -o yaml | grep 'claimName: mypv-claim' | awk '{print $2}') != 'mypv-claim' \
-&& $(kubectl get po ng-pod -o jsonpath='{.spec.containers[0].volumeMounts[0].mountPath}') != '/mnt/myvol' ]]
+if [[ $(kubectl get po ng-pod -o yaml | grep 'claimName: mypv-claim' | awk '{print $2}') == 'mypv-claim' \
+&& $(kubectl get po ng-pod -o jsonpath='{.spec.containers[0].volumeMounts[0].mountPath}') == '/mnt/myvol' ]]
 then
   exit 0
 else
