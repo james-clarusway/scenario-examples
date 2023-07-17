33f3027ea5b0b582c4e1131f4becd2db27933bc4
james-clarusway <james@clarusway.com>
2023-07-17 13:32:32 +0300

    mcp-s2-2

--- CKAD/multi-container-pods/step2/verify.sh
+++ CKAD/multi-container-pods/step2/verify.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-if [[ $(kubectl exec init-pod -- cat /usr/share/nginx/html/index.html) = 'Defaulted container "nginx" out of: nginx, install (init)
-<html><head></head><body><header>
-<title>http://info.cern.ch</title>
-</header>
+kubectl exec init-pod -- cat /usr/share/nginx/html/index.html > /mnt/index.html
 
-<h1>http://info.cern.ch - home of the first website</h1>
-<p>From here you can:</p>
-<ul>
-<li><a href="http://info.cern.ch/hypertext/WWW/TheProject.html">Browse the first website</a></li>
-<li><a href="http://line-mode.cern.ch/www/hypertext/WWW/TheProject.html">Browse the first website using the line-mode browser simulator</a></li>
-<li><a href="http://home.web.cern.ch/topics/birth-web">Learn about the birth of the web</a></li>
-<li><a href="http://home.web.cern.ch/about">Learn about CERN, the physics laboratory where the web was born</a></li>
-</ul>
-</body></html>' ]]
+if cmp -s "/mnt/cern.html" "/mnt/index.html"
 then
   exit 0
 else
