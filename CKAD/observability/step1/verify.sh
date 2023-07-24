588636cda43603341057ee0d0858dc10e3149fc3
james-clarusway <james@clarusway.com>
2023-07-24 16:36:58 +0300

    probes-3

--- CKAD/observability/step1/verify.sh
+++ CKAD/observability/step1/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(kubectl get po liveness-pod -o jsonpath='{.spec.containers[0].image}') = 'nginx' \
-&& $(kubectl get po liveness-pod -o jsonpath='{.spec.containers[0].livenessProbe.exec.command}') = 'ls' \
+&& $(kubectl get po liveness-pod -o jsonpath='{.spec.containers[0].livenessProbe.exec.command}') = '["ls"]' \
 && $(kubectl get po liveness-pod -o jsonpath='{.spec.containers[0].livenessProbe.initialDelaySeconds}') -eq 5 \
 && $(kubectl get po liveness-pod -o jsonpath='{.spec.containers[0].livenessProbe.periodSeconds}') -eq 5 ]]
 then
