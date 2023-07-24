bc72f7838cb51ecb0b42a9c42b70a5d57b193a2a
james-clarusway <james@clarusway.com>
2023-07-24 16:30:51 +0300

    probes-3

--- CKAD/observability/step3/verify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(kubectl get po ng-pod -o yaml | grep 'claimName: mypv-claim' | awk '{print $2}') == 'mypv-claim' \
-&& $(kubectl get po ng-pod -o jsonpath='{.spec.containers[0].volumeMounts[0].mountPath}') == '/mnt/myvol' ]]
-then
-  exit 0
-else
-  exit 1
-fi
