cb1e5f7589e4e6f99bf6e7ca2cd6a9490a94b7e4
james-clarusway <james@clarusway.com>
2023-07-27 12:26:36 +0300

    application-deploy-3

--- CKAD/application-deployment/step1/verify.sh
+++ CKAD/application-deployment/step1/verify.sh
@@ -2,7 +2,7 @@
 
 if [[ $(kubectl get deploy myapp -o jsonpath='{.spec.strategy.rollingUpdate.maxSurge}') = '10%' \
 && $(kubectl get deploy myapp -o jsonpath='{.spec.strategy.rollingUpdate.maxUnavailable}') = '5%' \
-$$ $(kubectl get deploy myapp -o jsonpath='{.spec.template.spec.containers[0].image}') = 'clarusway/clarusweb:2.0'
+&& $(kubectl get deploy myapp -o jsonpath='{.spec.template.spec.containers[0].image}') = 'clarusway/clarusweb:2.0'
 ]]
 then
   exit 0
