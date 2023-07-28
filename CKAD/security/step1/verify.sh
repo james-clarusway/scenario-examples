34d0f71fe38037fe669aa263d8d701bcbbc70edd
james-clarusway <james@clarusway.com>
2023-07-28 11:08:40 +0300

    security-1,

--- CKAD/security/step1/verify.sh
+++ CKAD/security/step1/verify.sh
@@ -4,9 +4,9 @@ if [[ $(kubectl get sa app-sa -n prod | grep -i app-sa | awk '{printf $1}') = 'a
 && $(kubectl get clusterrole app-clusterrole -o jsonpath='{.rules[0].apiGroups[0]}') = 'apps' \
 && $(kubectl get clusterrole app-clusterrole -o jsonpath='{.rules[0].resources[0]}') = 'deployments' \
 && $(kubectl get clusterrole app-clusterrole -o jsonpath='{.rules[0].verbs[0]}') = 'create' \
-&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding  -o jsonpath='{.roleRef'.name}') = 'app-clusterrole' \
-&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding  -o jsonpath='{.subjects[0].name}') = 'app-sa' \
-&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding  -o jsonpath='{.subjects[0].namespace}') = 'prod' ]]
+&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding -o jsonpath='{.roleRef.name}') = 'app-clusterrole' \
+&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding -o jsonpath='{.subjects[0].name}') = 'app-sa' \
+&& $(kubectl get clusterrolebindings.rbac.authorization.k8s.io app-clusterrolebinding -o jsonpath='{.subjects[0].namespace}') = 'prod' ]]
 then
   exit 0
 else
